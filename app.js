7797cd8f4f27db67ded26fd13cf5468770c05d1f
James Keicher <jamesdkeicher@gmail.com>
2021-12-01 15:12:55 -0800

    Refactored using 'event' of eventListener

--- app.js
+++ app.js
@@ -1,54 +1,49 @@
 // import functions and grab DOM elements
-import { makeStatsString } from './utils.js';
+import { makeStatsString, textFormatter } from './utils.js';
 const headDropdown = document.getElementById('head-dropdown');
 const middleDropdown = document.getElementById('middle-dropdown');
 const bottomDropdown = document.getElementById('bottom-dropdown');
-const headEl = document.getElementById('head');
-const middleEl = document.getElementById('middle');
-const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
 const catchphrasesEl = document.getElementById('chatchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
-// set state for how many times the user changes the head, middle, and bottom
-// set state for all of the character's catchphrases
-let catchPhrases = [];
-let head = 0;
-let middle = 0;
-let bottom = 0;
+let states = {
+    catchPhrases: [],
+    head: 0,
+    middle: 0,
+    bottom: 0
+};
 
-headDropdown.addEventListener('change', () => {
-    handleClick('head', headEl);
+headDropdown.addEventListener('change', (event) => {
+    handleClick(event);
 });
 
-
-middleDropdown.addEventListener('change', () => {
-    handleClick('middle', middleEl);
+middleDropdown.addEventListener('change', (event) => {
+    handleClick(event);
 });
 
-
-bottomDropdown.addEventListener('change', () => {
-    handleClick('bottom', bottomEl);
+bottomDropdown.addEventListener('change', (event) => {
+    handleClick(event);
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
     let phrase = catchphraseInput.value;
     // push the new catchphrase to the catchphrase array in state
-    catchPhrases.push(phrase);
+    states.catchPhrases.push(phrase);
     // update the dom for the bottom
     // catchphrasesEl.textContent = '';
     // clear out the form input's value so it's empty to the user
     catchphraseInput.value = '';
     // update the dom to show the new catchphrases (call a function to do this work)
-    displayCatchphrases(catchPhrases);
+    displayCatchphrases(states.catchPhrases);
 
 });
 
 function displayStats() {
     // change the text contentof the reportEl to tell the user how many times they've changed each piece of the state
-    const statsString = makeStatsString(head, middle, bottom); // call this function with the correct arguments
+    const statsString = makeStatsString(states.head, states.middle, states.bottom); // call this function with the correct arguments
     reportEl.textContent = statsString;
 }
 
@@ -56,48 +51,30 @@ function displayCatchphrases() {
     // clear out the DOM for the currently displayed catchphrases
     catchphrasesEl.textContent = '';
     // loop through each catchphrase in state
-    for (const phrase of catchPhrases) {
+    for (const phrase of states.catchPhrases) {
         let p = document.createElement('p');
         p.textContent = phrase;
         catchphrasesEl.append(p);
     }
 }
 
-const handleClick = (article, el) => {
-    let descriptor;
-    if (article === 'bottom') {
-        descriptor = 'pants';
-    } else {
-        descriptor = article;
-    }
-    let val;
-    let articles;
-    switch (article) {
-        case 'bottom':
-            val = bottomDropdown.value;
-            articles = () => {
-                bottom++;
-            };
-                
-            break;
-        case 'head':
-            val = headDropdown.value;
-            articles = () => {
-                head++;
-            };
-            break;
-        case 'middle':
-            val = middleDropdown.value;
-            articles = () => {
-                middle++;
-            };
-            break;
-    }
+const handleClick = (event) => {
+    let val = event.target.value;
+
+    let descriptor = textFormatter(event.target.id);
     
-    articles();
-    el.textContent = '';
+    states[descriptor]++;
+    
+    let domEl = document.getElementById(descriptor);
+
+    domEl.textContent = '';
     let newEl = document.createElement('img');
+
+    if (descriptor === 'bottom') {
+        descriptor = 'pants';
+    }
+
     newEl.src = `./assets/${val}-${descriptor}.png`;
-    el.append(newEl);
+    domEl.append(newEl);
     displayStats();
 };
--- utils.js
+++ utils.js
@@ -1,3 +1,17 @@
 export function makeStatsString(headCount, middleCount, bottomCount) {
     return `You clicked on the head ${headCount} times, on the middle ${middleCount} times, and on the bottom ${bottomCount} time. And everybody loves your character's catchphrases:`;
 }
+
+export function textFormatter(text){
+    let str = '';
+    for (let i = 0; i < text.length; i++) {
+        const element = text[i];
+        if (element !== '-') {
+            str += element;
+        } else {
+            break;
+        }
+        
+    }
+    return str;
+}
